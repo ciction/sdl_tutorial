7e14be820d4f4c80652141614e53ec6adcd3d1dc
ciction <swolfschristophe@gmail.com>
2017-12-11 18:20:41 +0100

    copied the source from orignal git tutorial and added lib file to linker - cleanup code in seperate include header is nice

--- tutorial_6/tutorial6.cpp
+++ tutorial_6/tutorial6.cpp
@@ -1,102 +1,50 @@
-//Lesson 6: True Type Fonts with SDL_ttf
-//http://www.willusher.io/sdl2%20tutorials/2013/12/18/lesson-6-true-type-fonts-with-sdl_ttf
-
 #include "stdafx.h"
 #include <iostream>
-#include "SDL.h"
+#include <string>
+#include <SDL.h>
+#include <SDL_ttf.h>
 #include "res_path.h"
-#include "SDL_image.h"
+#include "cleanup.h"
 
+/*
+* Lesson 6: True Type Fonts with SDL_ttf
+*/
+//Screen attributes
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-//eigen implementatie van cleanup
-template <class T>
-void cleanup(T *t) { }  // in general do nothing 
-
-template <>
-void cleanup(SDL_Texture *t) { SDL_DestroyTexture(t); }
-
-template <>
-void cleanup(SDL_Renderer *t) { SDL_DestroyRenderer(t); }
-
-template <>
-void cleanup(SDL_Window *t) { SDL_DestroyWindow(t); }
-
-
-/**
+/*
 * Log an SDL error with some error message to the output stream of our choice
-* @param os The output stream to write the message to
+* @param os The output stream to write the message too
 * @param msg The error message to write, format will be msg error: SDL_GetError()
 */
 void logSDLError(std::ostream &os, const std::string &msg) {
 	os << msg << " error: " << SDL_GetError() << std::endl;
-	std::cin.get();
 }
-
-
-/**
-* Loads a BMP image into a texture on the rendering device
-* @param file The BMP image file to load
-* @param ren The renderer to load the texture onto
-* @return the loaded texture, or nullptr if something went wrong.
-*/
-SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
-	SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
-	if (texture == nullptr) {
-		logSDLError(std::cout, "LoadTexture");
-	}
-	return texture;
-}
-
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
-* width and height
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-* @param w The width of the texture to draw
-* @param h The height of the texture to draw
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h) {
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	dst.w = w;
-	dst.h = h;
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
-/**
+/*
 * Draw an SDL_Texture to an SDL_Renderer at some destination rect
 * taking a clip of the texture if desired
 * @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param dst The destination rectangle to render the texture to
+* @param rend The renderer we want to draw too
+* @param dst The destination rectangle to render the texture too
 * @param clip The sub-section of the texture to draw (clipping rect)
 *		default of nullptr draws the entire texture
 */
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
-	SDL_Rect *clip = nullptr)
-{
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst, SDL_Rect *clip = nullptr) {
 	SDL_RenderCopy(ren, tex, clip, &dst);
 }
-
-/**
+/*
 * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
 * the texture's width and height and taking a clip of the texture if desired
-* If a clip is passed, the clip's width and height will be used instead of
-*	the texture's
+* If a clip is passed, the clip's width and height will be used instead of the texture's
 * @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
+* @param rend The renderer we want to draw too
+* @param x The x coordinate to draw too
+* @param y The y coordinate to draw too
 * @param clip The sub-section of the texture to draw (clipping rect)
 *		default of nullptr draws the entire texture
 */
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
-	SDL_Rect *clip = nullptr)
-{
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip = nullptr) {
 	SDL_Rect dst;
 	dst.x = x;
 	dst.y = y;
@@ -109,8 +57,7 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
 	}
 	renderTexture(tex, ren, dst, clip);
 }
-
-/**
+/*
 * Render the message we want to display to a texture for drawing
 * @param message The message we want to display
 * @param fontFile The font we want to use to render the text
@@ -119,8 +66,8 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
 * @param renderer The renderer to load the texture in
 * @return An SDL_Texture containing the rendered message, or nullptr if something went wrong
 */
-SDL_Texture* renderText(const std::string &message, const std::string &fontFile,
-	SDL_Color color, int fontSize, SDL_Renderer *renderer)
+SDL_Texture* renderText(const std::string &message, const std::string &fontFile, SDL_Color color,
+	int fontSize, SDL_Renderer *renderer)
 {
 	//Open the font
 	TTF_Font *font = TTF_OpenFont(fontFile.c_str(), fontSize);
@@ -128,8 +75,8 @@ SDL_Texture* renderText(const std::string &message, const std::string &fontFile,
 		logSDLError(std::cout, "TTF_OpenFont");
 		return nullptr;
 	}
-	//We need to first render to a surface as that's what TTF_RenderText
-	//returns, then load that surface into a texture
+	//We need to first render to a surface as that's what TTF_RenderText returns, then
+	//load that surface into a texture
 	SDL_Surface *surf = TTF_RenderText_Blended(font, message.c_str(), color);
 	if (surf == nullptr) {
 		TTF_CloseFont(font);
@@ -146,151 +93,77 @@ SDL_Texture* renderText(const std::string &message, const std::string &fontFile,
 	return texture;
 }
 
-
-int main(int argc, char* argv[])
-{
-
-	//iW and iH are the clip width and height
-	//We'll be drawing only clips so get a center position for the w/h of a clip
-	int iW = 100, iH = 100;
-	const int SCREEN_WIDTH = 640;
-	const int SCREEN_HEIGHT = 480;
-	const int TILE_SIZE = 40;
-
-	//Setup the clips for our image
-	SDL_Rect clips[4];
-	for (int i = 0; i < 4; ++i) {
-		clips[i].x = i / 2 * iW;
-		clips[i].y = i % 2 * iH;
-		clips[i].w = iW;
-		clips[i].h = iH;
-	}
-	//Specify a default clip to start with
-	int useClip = 0;
-
-	//try to start SDL
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+int main(int, char**) {
+	//Start up SDL and make sure it went ok
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		logSDLError(std::cout, "SDL_Init");
 		return 1;
 	}
-
-	//init PNG subsystem (so the first PNG images will load faster )
-	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
-		logSDLError(std::cout, "IMG_Init");
-		SDL_Quit();
-		return 1;
-	}
-
-	//try to init ttf font lib
+	//Also need to init SDL_ttf
 	if (TTF_Init() != 0) {
 		logSDLError(std::cout, "TTF_Init");
 		SDL_Quit();
 		return 1;
 	}
 
-	//try opening a window
-	SDL_Window *window = SDL_CreateWindow("	n 4", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	//Setup our window and renderer
+	SDL_Window *window = SDL_CreateWindow("Lesson 6", SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr) {
-		logSDLError(std::cout, "SDL_CreateWindow");
+		logSDLError(std::cout, "CreateWindow");
+		TTF_Quit();
 		SDL_Quit();
 		return 1;
 	}
-
-	//try creating the renderer
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (renderer == nullptr) {
 		logSDLError(std::cout, "CreateRenderer");
-		cleanup(window);
-		SDL_Quit();
-		return 1;
-	}
-
-
-
-	const std::string resPath = getResourcePath("Lesson6");
-	//We'll render the string "TTF fonts are cool!" in white
-	//Color is in RGBA format
-	SDL_Color color = { 255, 255, 255, 255 };
-	SDL_Texture *image = renderText("TTF fonts are cool!", resPath + "sample.ttf",
-		color, 64, renderer);
-	if (image == nullptr) {
-		cleanup(renderer);
 		cleanup(window);
 		TTF_Quit();
 		SDL_Quit();
 		return 1;
 	}
+
+	const std::string resPath = getResourcePath("Lesson6");
+	//We'll render the string "TTF fonts are cool!" in white
+	//Color is in RGB format
+	SDL_Color color = { 255, 255, 255, 255 };
+	SDL_Texture *image = renderText("TTF fonts are cool!", resPath + "roboto-regular.ttf", color, 64, renderer);
+	if (image == nullptr) {
+		cleanup(image, renderer, window);
+		TTF_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
 	//Get the texture w/h so we can center it in the screen
 	int iW, iH;
 	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
 	int x = SCREEN_WIDTH / 2 - iW / 2;
 	int y = SCREEN_HEIGHT / 2 - iH / 2;
 
-
-	//real render loop
-	//Our event structure
 	SDL_Event e;
 	bool quit = false;
 	while (!quit) {
+		//Event Polling	
 		while (SDL_PollEvent(&e)) {
-
-			switch (e.key.keysym.sym) {
-			case SDLK_1:
-			case SDLK_KP_1:
-				useClip = 0;
-				break;
-			case SDLK_2:
-			case SDLK_KP_2:
-				useClip = 1;
-				break;
-			case SDLK_3:
-			case SDLK_KP_3:
-				useClip = 2;
-				break;
-			case SDLK_4:
-			case SDLK_KP_4:
-				useClip = 3;
-				break;
-			case SDLK_ESCAPE:
+			if (e.type == SDL_QUIT) {
 				quit = true;
-				break;
-			default:
-				break;
+			}
+			if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) {
+				quit = true;
 			}
 		}
-
-		//Render the scene
-		//clear screen
-		SDL_RenderClear(renderer);
-
-	
-		//Note: This is within the program's main loop
 		SDL_RenderClear(renderer);
 		//We can draw our message as we do any other texture, since it's been
 		//rendered to a texture
 		renderTexture(image, renderer, x, y);
 		SDL_RenderPresent(renderer);
-
-
-		//present
-		SDL_RenderPresent(renderer);
 	}
-
-	std::cout << "Resource path is: " << getResourcePath() << std::endl;
-
-
-	//cleanup
-
-	cleanup(renderer);
-	cleanup(window);
-	cleanup(image);
+	//Clean up
+	cleanup(image, renderer, window);
+	TTF_Quit();
 	SDL_Quit();
 
-	//std::cin.get();
-	SDL_Quit();
 	return 0;
-
-
 }
-
-
